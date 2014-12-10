debug: minMaxAng function give wrong result.

Geometry.py
@@ -240,8 +240,7 @@ class Geometry(object):
         x = np.mean(np.cos(ang))
         y = np.mean(np.sin(ang))
         ang0 = np.arctan2(y, x)
-        ang -= ang0 + np.pi
-        ang %= (2 * np.pi)
+        ang = ((ang - ang0) % (2 * np.pi) + np.pi) % (2 * np.pi)
         minAng = (ang.min() + ang0 - np.pi) % (2 * np.pi)
         maxAng = (ang.max() + ang0 - np.pi) % (2 * np.pi)
         return minAng, maxAng

testGeometry.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import cv2
 import numpy as np
-import Plot
 
+import Plot
 import Geometry
 
 
@@ -87,11 +87,11 @@ if __name__ == "__main__1":
     errorMax = 1e-6
     for i in range(1000):
         geo = Geometry.Geometry()
-        ang1 = np.random.randint(-360, 360)
-        ang2 = ang1 + np.random.randint(27)
+        ang1 = np.random.randint(360)
+        ang2 = ang1 + np.random.randint(180)
         angs = np.deg2rad(np.arange(ang1, ang2 + 1)) % (2 * np.pi)
         minAng, maxAng = np.rad2deg(geo.minMaxAng(angs))
-        if abs(minAng - (ang1 % 360)) > errorMax or abs(maxAng - (ang2 % 360)) > errorMax:
+        if abs((minAng % 360) - (ang1 % 360)) > errorMax or abs((maxAng % 360) - (ang2 % 360)) > errorMax:
             print "Error !"
-            print minAng, maxAng, ang1, ang2
+            print minAng, ang1, maxAng, ang2
     print "Tested."