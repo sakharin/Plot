8cfaf6e2c50c33a47cafb78e493c7f049106cdfb
Sakharin Buachan <sakharin@gmail.com>
2015-03-27 13:41:45 +0900

    Rearrange testCases.cpp.

--- testCases.cpp
+++ testCases.cpp
@@ -7,6 +7,8 @@
 #define TWOPI 6.28318530718
 #define PIOTWO 1.57079632679
 
+#define H 100
+#define W 120
 
 void test_vec2Angs();
 void test_vec2Angs_Vec();
@@ -54,36 +56,30 @@ void test_vec2Angs_Vec() {
 
 void test_vec2Angs_Mat() {
     Geometry geo = Geometry();
-    int h = 6, w = 10;
-    cv::Mat vecs = cv::Mat::zeros(h, w, CV_32FC3);
-    cv::Mat thetas = cv::Mat::zeros(h, w, CV_32F);
-    cv::Mat phis = cv::Mat::zeros(h, w, CV_32F);
+    cv::Mat vecs = cv::Mat::zeros(H, W, CV_32FC3);
+    cv::Mat thetas = cv::Mat::zeros(H, W, CV_32F);
+    cv::Mat phis = cv::Mat::zeros(H, W, CV_32F);
+    cv::randu(vecs, cv::Scalar(-1000), cv::Scalar(1000));
+
+    geo.vec2Angs(&vecs, &thetas, &phis);
 
     float *p;
-    float x, y, z;
-    for(int i = 0; i < h; i++) {
+    float x, y, z, r;
+    for(int i = 0; i < H; i++) {
         p = vecs.ptr<float>(i);
-        for(int j = 0; j < w; j++) {
-            x = 1;
-            y = std::sin((w / 2. - j) / 360. * TWOPI);
-            z = h / 2. - i;
-            (*p++) = x;
-            (*p++) = y;
-            (*p++) = z;
-        }
-    }
-    geo.vec2Angs(&vecs, &thetas, &phis);
-    float r;
-    for(int i = 0; i < h; i++) {
-        for(int j = 0; j < w; j++) {
-            x = 1;
-            y = std::sin((w / 2. - j) / 360. * TWOPI);
-            z = h / 2. - i;
+        for(int j = 0; j < W; j++) {
+            x = *p++;
+            y = *p++;
+            z = *p++;
 
             r = std::sqrt(x * x + y * y + z * z);
-            assert(std::abs(thetas.at<float>(i, j) - std::acos(z / r)) < 0.000001);
-
-            assert(std::abs(phis.at<float>(i, j) - std::fmod(std::atan2(y, x) + TWOPI, TWOPI)) < 0.000001);
+            if(r == 0) {
+                assert(std::abs(thetas.at<float>(i, j)) < 0.000001);
+                assert(std::abs(phis.at<float>(i, j)) < 0.000001);
+            } else {
+                assert(std::abs(thetas.at<float>(i, j) - std::acos(z / r)) < 0.000001);
+                assert(std::abs(phis.at<float>(i, j) - std::fmod(std::atan2(y, x) + TWOPI, TWOPI)) < 0.000001);
+            }
         }
     }
 }
@@ -95,18 +91,19 @@ void test_twoPts2Vec() {
 
 void test_twoPts2Vec_PtPt() {
     Geometry geo = Geometry();
-    float arr1[] = {1, 2, 3};
-    float arr2[] = {7, 8, 9};
-    cv::Mat P1 = cv::Mat(3, 1, CV_32F, arr1);
-    cv::Mat P2 = cv::Mat(3, 1, CV_32F, arr2);
+    cv::Mat P1 = cv::Mat(3, 1, CV_32F);
+    cv::Mat P2 = cv::Mat(3, 1, CV_32F);
     cv::Mat vec = cv::Mat::zeros(3, 1, CV_32F);
+    cv::randu(P1, cv::Scalar(-1000), cv::Scalar(1000));
+    cv::randu(P2, cv::Scalar(-1000), cv::Scalar(1000));
 
     geo.twoPts2Vec(P1, P2, &vec);
 
     float x, y, z, norm;
-    x = arr1[0] - arr2[0];
-    y = arr1[1] - arr2[1];
-    z = arr1[2] - arr2[2];
+    P1 = P1 - P2;
+    x = P1.at<float>(0, 0);
+    y = P1.at<float>(1, 0);
+    z = P1.at<float>(2, 0);
     norm = std::sqrt(x * x + y * y + z * z);
     x /= norm;
     y /= norm;
@@ -119,12 +116,11 @@ void test_twoPts2Vec_PtPt() {
 
 void test_twoPts2Vec_PtMat() {
     Geometry geo = Geometry();
-    int h = 100, w = 120;
     cv::Mat P1 = cv::Mat(3, 1, CV_32F);
-    cv::Mat P2 = cv::Mat(h, w, CV_32F);
-    cv::Mat vec = cv::Mat::zeros(h, w, CV_32FC3);
-    cv::randu(P1, cv::Scalar(-100), cv::Scalar(10));
-    cv::randu(P2, cv::Scalar(-100), cv::Scalar(10));
+    cv::Mat P2 = cv::Mat(H, W, CV_32F);
+    cv::Mat vec = cv::Mat::zeros(H, W, CV_32FC3);
+    cv::randu(P1, cv::Scalar(-1000), cv::Scalar(1000));
+    cv::randu(P2, cv::Scalar(-1000), cv::Scalar(1000));
 
     float x1 = P1.at<float>(0, 0);
     float y1 = P1.at<float>(1, 0);
@@ -132,7 +128,7 @@ void test_twoPts2Vec_PtMat() {
     float x, y, z, norm;
     float *x2, *y2, *z2;
     float *x3, *y3, *z3;
-    for(int i =0; i < h; i++) {
+    for(int i =0; i < H; i++) {
         x2 = P2.ptr<float>(i);
         y2 = x2 + 1;
         z2 = x2 + 2;
@@ -140,7 +136,7 @@ void test_twoPts2Vec_PtMat() {
         x3 = vec.ptr<float>(i);
         y3 = x3 + 1;
         z3 = x3 + 2;
-        for(int j =0; j < h; j++) {
+        for(int j =0; j < W; j++) {
             x = x1 - *x2;
             y = y1 - *y2;
             z = z1 - *z2;
@@ -167,32 +163,43 @@ void test_twoPts2Angs() {
 
 void test_twoPts2Angs_PtPt() {
     Geometry geo = Geometry();
-    float arr1[] = {1, 2, 3};
-    float arr2[] = {7, 8, 9};
-    cv::Mat P1 = cv::Mat(3, 1, CV_32F, arr1);
-    cv::Mat P2 = cv::Mat(3, 1, CV_32F, arr2);
+    cv::Mat P1 = cv::Mat(3, 1, CV_32F);
+    cv::Mat P2 = cv::Mat(3, 1, CV_32F);
     float theta, phi;
 
-    geo.twoPts2Angs(P1, P2, &theta, &phi);
+    for(int i = 0; i < H; i++) {
+        for(int j = 0; j < W; j++) {
+            cv::randu(P1, cv::Scalar(-1000), cv::Scalar(1000));
+            cv::randu(P2, cv::Scalar(-1000), cv::Scalar(1000));
 
-    float x, y, z;
-    x = arr1[0] - arr2[0];
-    y = arr1[1] - arr2[1];
-    z = arr1[2] - arr2[2];
+            geo.twoPts2Angs(P1, P2, &theta, &phi);
 
-    assert(std::abs(phi - std::fmod(std::atan2(y, x) + TWOPI, TWOPI)) < 0.000001);
+            float x, y, z, r;
+            P1 = P1 - P2;
+            x = P1.at<float>(0, 0);
+            y = P1.at<float>(1, 0);
+            z = P1.at<float>(2, 0);
+
+            r = std::sqrt(x * x + y * y + z * z);
+            if(r == 0) {
+                assert(std::abs(theta) < 0.000001);
+                assert(std::abs(phi) < 0.000001);
+            } else {
+                assert(std::abs(theta - std::acos(z / r)) < 0.000001);
+                assert(std::abs(phi - std::fmod(std::atan2(y, x) + TWOPI, TWOPI)) < 0.000001);
+            }
+        }
+    }
 }
 
 void test_twoPts2Angs_PtMat() {
     Geometry geo = Geometry();
-    int h = 6, w = 10;
     cv::Mat P1 = cv::Mat(3, 1, CV_32F);
-    cv::Mat P2 = cv::Mat(h, w, CV_32FC3);
-    cv::Mat thetas = cv::Mat::zeros(h, w, CV_32F);
-    cv::Mat phis = cv::Mat::zeros(h, w, CV_32F);
-    cv::randu(P1, cv::Scalar(-100), cv::Scalar(10));
-    cv::randu(P2, cv::Scalar(-100), cv::Scalar(10));
-
+    cv::Mat P2 = cv::Mat(H, W, CV_32FC3);
+    cv::Mat thetas = cv::Mat::zeros(H, W, CV_32F);
+    cv::Mat phis = cv::Mat::zeros(H, W, CV_32F);
+    cv::randu(P1, cv::Scalar(-1000), cv::Scalar(1000));
+    cv::randu(P2, cv::Scalar(-1000), cv::Scalar(1000));
 
     float x1 = P1.at<float>(0, 0);
     float y1 = P1.at<float>(1, 0);
@@ -202,9 +209,9 @@ void test_twoPts2Angs_PtMat() {
 
     float x, y, z, r;
     float *p2;
-    for(int i = 0; i < h; i++) {
+    for(int i = 0; i < H; i++) {
         p2 = P2.ptr<float>(i);
-        for(int j = 0; j < w; j++) {
+        for(int j = 0; j < W; j++) {
             x = x1 - *p2++;
             y = y1 - *p2++;
             z = z1 - *p2++;
